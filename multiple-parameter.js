fab786618cd6f3f94ae59350142b63479d8bdfb1
Md Sulayman Hosen Abir <73769735+Sh75cr7@users.noreply.github.com>
2021-08-23 18:00:46 +0600

    first commit

---
+++ multiple-parameter.js
@@ -0,0 +1,34 @@
+// function addTwoNumbers(number1, number2) {
+//     console.log(number1, number2);
+//     var total = number1 + number2;
+//     return total;
+// }
+
+// var firstNumber = 35;
+// var secondNumber = 47;
+// var result = addTwoNumbers(secondNumber, firstNumber);
+// console.log('result value: ', result);
+
+
+// function multiplyTwoNumbers(num1, num2) {
+//     var result = num1 * num2;
+//     return result;
+// }
+
+// var total = multiplyTwoNumbers(5, 100);
+// console.log('total after multiplication', total);
+
+// biyog koro duita sonkha
+function minusNumbers(number1, number2){
+    console.log(number1, number2);
+    var result= number1-number2;
+    return result;
+}
+var total=minusNumbers(56 , 50);
+var result= number1-number2;
+console.log(result);
+
+
+
+// vabfol ber koro
+
